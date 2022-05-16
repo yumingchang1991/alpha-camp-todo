b7e486dbd37aa31d0173537195e6d3f4b93fdddb
ChillDiver <yumingchang1991@gmail.com>
2022-05-16 21:21:00 +0800

    initialize handlebars

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoDb = require('./connection.js')
-const todoSchema = require('./models/todo.js')
+const Todo = require('./models/todo.js')
 const { engine } = require('express-handlebars')
+const { find } = require('./models/todo.js')
 const app = express()
 const port = 3000
 
@@ -10,6 +11,12 @@ app.set('view engine', 'handlebars')
 
 app
   .route('/')
-  .get((req, res) => res.send('Express is live'))
+  .get((req, res) => {
+    Todo
+      .find()
+      .lean()
+      .then( todos => res.render('index', { todos }))
+      .catch( error => console.error(error))
+  })
 
 app.listen(port, () => console.log(`Express is listening on localhost:${port}`))
