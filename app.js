f35e732b6d08513a5f3bc0c36c749f6e2ba859a4
ChillDiver <yumingchang1991@gmail.com>
2022-05-16 17:16:48 +0800

    initialize project files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app
+  .route('/')
+  .get((req, res) => res.send('Express is live'))
+
+app.listen(port, () => console.log(`Express is listening on localhost:${port}`))
