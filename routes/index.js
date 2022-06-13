251c00b489fd19f4f87919b655dd9b6285d3d6e3
ChillDiver <yumingchang1991@gmail.com>
2022-06-13 15:16:33 +0800

    feat: add route: users/login and a sample login page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 module.exports = router
