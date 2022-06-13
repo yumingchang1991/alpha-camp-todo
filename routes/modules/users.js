f4a94b1cc9dc2e73083f5efb1f123209ef8a0e18
ChillDiver <yumingchang1991@gmail.com>
2022-06-13 15:21:32 +0800

    feat: add user login template and user register route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,5 +3,11 @@ const router = require('express').Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.post('/login', (req, res) => {
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 module.exports = router
