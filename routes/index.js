1e7bae8e52cabe800341a5c52680d02f86aeb01f
ChillDiver <yumingchang1991@gmail.com>
2022-06-14 14:09:28 +0800

    feat: add authentication middleware

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = require('express').Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
