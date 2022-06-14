cc25216d3ae95616edba735aa8b76aa297d3c5bd
ChillDiver <yumingchang1991@gmail.com>
2022-06-14 13:52:53 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -40,4 +40,9 @@ router.post('/register', (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 module.exports = router
