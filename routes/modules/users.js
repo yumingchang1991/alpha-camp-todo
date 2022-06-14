658197f5754a518e6784cca99a915fe733cbf817
ChillDiver <yumingchang1991@gmail.com>
2022-06-14 15:24:24 +0800

    fix: req.logout needs a callback since v0.6

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -41,8 +41,10 @@ router.post('/register', (req, res) => {
 })
 
 router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/users/login')
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/users/login')
+  });
 })
 
 module.exports = router
