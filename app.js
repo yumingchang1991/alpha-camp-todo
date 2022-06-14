57a4c9ebfe065c1133445f6ee5adcf7f686c72cb
ChillDiver <yumingchang1991@gmail.com>
2022-06-14 15:38:18 +0800

    feat: add conditional rendering nav bar

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
 app.use(routes)
 
